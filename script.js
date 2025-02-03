df24e94d5d470b9a6f69fdcaf9b0f9427eda06c8
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-03 17:33:04 +0400

    add toggle

--- script.js
+++ script.js
@@ -8,16 +8,15 @@ let active=null;
 questions.forEach(question => {
     question.addEventListener('click',()=> {
         let questionDiv=question.closest('.question-div')
+        questionDiv.classList.toggle('active')
         if(questionDiv) {
             if(questionDiv === active) {
-                questionDiv.classList.remove('active')
                 active=null;
             } else {
                 if(active) {
                     active.classList.remove('active')
                     active=questionDiv
                 }
-                questionDiv.classList.add('active')
                 active=questionDiv         
             }
         }
