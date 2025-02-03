4a7faf55d22054242d2cce6625defd0d659c1449
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-03 17:27:43 +0400

    Implemented click event listeners on all elements with the .question-div class.

--- script.js
+++ script.js
@@ -7,28 +7,18 @@ let active=null;
 
 questions.forEach(question => {
     question.addEventListener('click',()=> {
-        let closest=question.closest('.question-div')
-        if(closest) {
-            let Question=closest.children[0].children[0]
-            let Arrow=closest.children[0].children[1]
-            let Answer=closest.children[1]
-
-            if(closest === active) {
-                Question.style.fontWeight='normal'
-                Arrow.style.transform='rotate(0)'
-                Answer.style.display='none'
+        let questionDiv=question.closest('.question-div')
+        if(questionDiv) {
+            if(questionDiv === active) {
+                questionDiv.classList.remove('active')
                 active=null;
             } else {
                 if(active) {
-                    active.children[0].children[0].style.fontWeight='normal'
-                    active.children[0].children[1].style.transform='rotate(0)'
-                    active.children[1].style.display='none'
-                    active=null
+                    active.classList.remove('active')
+                    active=questionDiv
                 }
-                Question.style.fontWeight='bold'
-                Arrow.style.transform='rotate(180deg)'
-                Answer.style.display='block'
-                active=closest;
+                questionDiv.classList.add('active')
+                active=questionDiv         
             }
         }
     })
